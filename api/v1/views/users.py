c9b324f6b201ef28fa80f63eb1521709d9a18a34
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-28 18:16:40 -0700

    Test

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,89 +1,74 @@
 #!/usr/bin/python3
-""" AirBnB users Index page """
-from api.v1.views import app_views
-from flask import jsonify, abort, request
+"""
+View for Users that handles all RESTful API actions
+"""
+
+from flask import jsonify, request, abort
 from models import storage
 from models.user import User
-from datetime import datetime
-import uuid
+from api.v1.views import app_views
 
 
-@app_views.route('/users/', methods=['GET', 'POST'])
-@app_views.route('users/<user_id>', methods=['GET', 'DELETE', 'PUT'])
-def handle_users_routes(user_id=None):
-    """ Handle user RESTFul API actions """
-    if request.method == 'GET':
-        return get_users(user_id)
-    elif request.method == 'POST':
-        return create_user()
-    elif request.method == 'PUT':
-        return update_user(user_id)
-    elif request.method == 'DELETE':
-        return delete_user(user_id)
-    else:
-        abort(405)
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def users_all():
+    """ returns list of all User objects """
+    users_all = []
+    users = storage.all(User).values()
+    for user in users:
+        users_all.append(user.to_json())
+    return jsonify(users_all)
 
 
-def get_users(user_id):
-    """ handle GET request """
-    if user_id is None:
-        users = [user.to_dict() for user in storage.all("User").values()]
-        return jsonify(users)
-    else:
-        user = storage.get(User, user_id)
-
-        if user is None:
-            abort(404)
-
-    return jsonify(user.to_dict())
-
-
-def create_user():
-    """ handle POST request"""
-    data = request.get_json()
-
-    if data is None:
-        abort(400, "Not a JSON")
-    if 'email' not in data:
-        abort(400, 'Missing email')
-    if 'password' not in data:
-        abort(400, 'Missing password')
-
-    user = User(**data)
-    user.save()
-    return jsonify(user.to_dict()), 201
-
-
-def update_user(user_id):
-    """ handle UPDATE request"""
+@app_views.route('/users/<user_id>', methods=['GET'])
+def user_get(user_id):
+    """ handles GET method """
     user = storage.get(User, user_id)
-
     if user is None:
         abort(404)
-
-    data = request.get_json()
-
-    if data is None:
-        abort(400, "Not a JSON")
-
-    for key, value in data.items():
-        invalid_keys = ["id", "email", "created_at", "updated_at"]
-
-        if key not in invalid_keys:
-            setattr(user, key, value)
-
-    user.save()
-
-    return jsonify(user.to_dict()), 200
+    user = user.to_json()
+    return jsonify(user)
 
 
-def delete_user(user_id):
-    """ handle DELETE request"""
-    user = storage.get(User, user_id)
-
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def user_delete(user_id):
+    """ handles DELETE method """
+    user = storage.get("User", user_id)
     if user is None:
         abort(404)
-
     storage.delete(user)
     storage.save()
     return jsonify({}), 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def user_post():
+    """ handles POST method """
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    if 'email' not in data:
+        abort(400, "Missing email")
+    if 'password' not in data:
+        abort(400, "Missing password")
+    user = User(**data)
+    user.save()
+    user = user.to_json()
+    return jsonify(user), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def user_put(user_id):
+    """ handles PUT method """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    for key, value in data.items():
+        ignore_keys = ["id", "email", "created_at", "updated_at"]
+        if key not in ignore_keys:
+            user.bm_update(key, value)
+    user.save()
+    user = user.to_json()
+    return jsonify(user), 200
