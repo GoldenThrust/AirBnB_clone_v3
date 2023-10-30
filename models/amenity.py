a90109fc0f1172da3f89bcb541c4026578a3bfdd
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-30 13:39:36 -0700

    Revert "No COmmit Message"
    This reverts commit d11429a9047fb49b21d965ce77a8ebbc532798a8.

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,21 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from os import getenv
+#!/usr/bin/python
+""" holds class Amenity"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from models.place import place_amenity
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """ The amenity class, contain name """
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = relationship("Place", secondary=place_amenity,
-                                       back_populates="amenities")
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
