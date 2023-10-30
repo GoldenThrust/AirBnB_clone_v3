2af4db7bef574cd985b1785b6888948939a31be3
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-10-30 08:42:40 -0700

    No Commit Message

--- models/user.py
+++ models/user.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from os import getenv
+""" holds class User"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        places = relationship('Place',  cascade='all, delete, delete-orphan',
-                              backref='user')
-        reviews = relationship('Review',  cascade='all, delete, delete-orphan',
-                               backref='user')
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user", cascade="all, delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
